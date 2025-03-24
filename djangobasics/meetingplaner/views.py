04598c3d33f404da5a9df07096ae8e6351bb5651
tanwardivya <divyatanwar26@gmail.com>
2025-03-24 14:16:14 -0700

    Additional Functionalities URL and Link Building

--- djangobasics/meetingplaner/views.py
+++ djangobasics/meetingplaner/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render,get_object_or_404
-from meetingplaner.models import Meetings
+from meetingplaner.models import Meetings, Room
 
 # Create your views here.
 def detail(request,id):
     # meeting = get_object_or_404(Meetings, pk=id)
     meeting = Meetings.objects.get(pk = id)
     return render(request, "meetingplaner/detail.html",{"meeting":meeting})
+
+#Please add a new page that shows a list of all room objects.
+#The page should be accessible via the URL /rooms.
+
+def rooms_list(request):
+    return render(request, "meetingplaner/rooms_list.html",{"rooms":Room.objects.all()})
